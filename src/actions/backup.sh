f6a789b14088ee3b0ca216bd5dca97f771a4b443
[Wikodit] Romain <WikRomain@Mac-mini-de-WIKOTV-01.local>
2023-03-15 12:10:33 +0100

    feat(WIP): call prune after backup

--- src/actions/backup.sh
+++ src/actions/backup.sh
@@ -1,7 +1,10 @@
 # Backup entrypoint
-backup () {
+backup() {
   call validate_config_${ADAPTER}
   call prepare_restic_args
   call ensure_repository true
   call "backup_${ADAPTER}" && info "backup done" || exception "backup failed, enable verbose with -v or debug with -d" $? 2
+  if [[! "$WAIOB_DISABLE_AUTO_CLEAN" -eq 1 ]]; then
+    call prune --last=1
+  fi
 }
